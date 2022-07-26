1984dfeccf9ca03494137b52c315f54fec9d6795
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-26 13:25:45 -0300

    ternary operator test

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,13 @@
+package operadorternario;
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        int n1, n2, maior;
+        n1 = 10;
+        n2 = 20;
+        maior = (n1>n2)?n1:n2;
+        System.out.println(maior);
+    }
+    
+}
